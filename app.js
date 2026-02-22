3033bd571fa76d307482faacbf9c85c18e66571d
myesha-shoaib <myeshashoaib@gmail.com>
2026-02-22 12:32:40 +0500

    Updated app.js with new changes

--- app.js
+++ app.js
@@ -1,3 +1,91 @@
-window.alert("hello everyone");
-alert(6+9);     
-document.getElementById("heading1").innerHTML="its my website";
+// window.alert("hello everyone");
+// alert(6+9);     
+// document.getElementById("heading1").innerHTML="its my website";
+// var myName;
+// myName = 123;
+
+// var myname = 23;
+
+// console.log(myName);
+// console.log(myname);
+
+// document.writeln(myName);
+// document.writeln(myname);
+
+// TASK ONE
+
+var num1 = 5;
+var num2 = 4;
+var sum= num1 + num2;
+console.log(sum);
+
+// TASK TWO
+
+var a = 10;
+var b = 20;
+var result = a + b;
+console.log(result);
+
+// TASK THREE
+var lenghtandwidth = 5;
+var variablearea = 3;
+var sum= lenghtandwidth * variablearea;
+console.log(sum)
+
+// TASK FORTH
+var x = 15;
+var y = 5;
+var answer = x -y;
+console.log(answer);
+
+// TASK FIVE
+var totalmarks = 100;
+var obtainedMarks = 78;
+var subtraction= totalmarks - obtainedMarks;
+console.log(subtraction)
+
+// TASK SIXTH
+var num1 = 6;
+var num2 = 7;
+var product = num1 * num2;
+console.log(product);
+
+// TASK SEVEN
+let myName ="myesha";
+const myage = 12;
+
+console.log(myName);
+console.log(myage);
+
+// TASK EIGHT
+let great ="hello";
+let my_Name ="Myesha"
+let age = 10;
+let myAge = age + 10;
+
+console.log(age - 2 + myAge);
+console.log(myAge);
+
+//  TASK NINE 
+// let num1 = 4;
+// let num2 = 8;
+// let sum = num1 + num2;
+// // OLDEST CONCENATION
+// document.writeln("sum of" +num1+ "and"+num2 +"is "+sum+"." );
+//  MODERN CONCENATION
+
+
+
+// TASK TEEN
+let firstName = "MYESHA";
+let favoriteFood = "Biryani";
+let city = "Karachi";
+let Age = 16;
+let favoriteNumber = 7;
+
+alert(`Hi, my name is ${firstName}
+I am ${Age} years old.
+I live in ${city}
+My favorite food is ${favoriteFood}
+My favorite number is ${favoriteNumber}
+`);
